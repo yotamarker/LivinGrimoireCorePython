a1f48564a20cb8c169885666a3e024b234f9a100
moti barski <yottaenkigo@yahoo.com>
2022-05-04 21:23:11 +0300

    init2

---
+++ livingrimoire1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'livingrimoire',
+    packages = ['livingrimoire1'],
+    version = '1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'AGI software design pattern',
+    author = 'moti barski',
+    author_email = '',
+    url = 'https://github.com/yotamarker/LivinGrimoireCorePython',
+    download_url = 'https://github.com/yotamarker/LivinGrimoireCorePython/archive/refs/tags/0.1.10.tar.gz',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
